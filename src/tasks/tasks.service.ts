4f058765946474c043a827f0fa4ca29182a25fff
Bassem <bassem@coradine.com>
2020-04-24 14:43:13 +0200

    Added Task model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './tasks.model';
 
 @Injectable()
 export class TasksService {
-    private tasks = [];
+  private tasks: Task[] = [];
 
-    getAllTasks() {
-        return this.tasks;
-    }
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 }
