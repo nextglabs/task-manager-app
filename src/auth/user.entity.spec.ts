96b445306bfcbc78de46cdc1dc4c1391c72fe6b9
Bassem <bassem@coradine.com>
2020-04-26 17:47:30 +0200

    Added user entity tests

---
+++ src/auth/user.entity.spec.ts
@@ -0,0 +1,34 @@
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
+
+describe('User entity', () => {
+  describe('validatePassword', () => {
+    let user;
+    beforeEach(() => {
+      user = new User();
+      user.salt = 'testSalt';
+      user.password = 'hashedPassword';
+      bcrypt.hash = jest.fn();
+    });
+
+    it('returns true when password matches hash', async () => {
+      bcrypt.hash.mockResolvedValue(user.password);
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+
+      const result = await user.validatePassword(user.password);
+      expect(bcrypt.hash).toHaveBeenCalledWith(user.password, user.salt);
+
+      expect(result).toEqual(true);
+    });
+
+    it("returns false when password doesn't match hash", async () => {
+      bcrypt.hash.mockResolvedValue('wrongHashedPassword');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+
+      const result = await user.validatePassword('wrongPassword');
+      expect(bcrypt.hash).toHaveBeenCalledWith('wrongPassword', user.salt);
+
+      expect(result).toEqual(false);
+    });
+  });
+});
