ed272c76246b530bc2126b1e61e3ab66b2a26802
Bassem <bassem@coradine.com>
2020-04-26 04:25:04 +0200

    Added config + cors in development mode

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,13 +6,16 @@ import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'topSecret2020',
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
       signOptions: {
-        expiresIn: 3600, // 1h validity
+        expiresIn: jwtConfig.expiresIn, // 1h validity
       },
     }),
     TypeOrmModule.forFeature([UserRepository]),
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
+  const serverConfig = config.get('server');
 
   const app = await NestFactory.create(AppModule);
 
-  const port = 4000;
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
